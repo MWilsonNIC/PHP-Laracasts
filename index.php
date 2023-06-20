372adaafe5f394d668f5367f896ffef78225a499
MWilsonNIC <mwilson@nic.bc.ca>
2023-06-19 18:41:27 -0700

    Add text and inline styles

--- index.php
+++ index.php
@@ -3,12 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
     <h1>
-        <?php 
        
-        ?>
+       <h1>
+       You have read 'Dark Matter'.
+       </h1>
+       
     </h1>
     
 </body>
