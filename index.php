56bb95b1a8a58944b1e39ed7a574b9a3553d270e
MWilsonNIC <mwilson@nic.bc.ca>
2023-06-19 19:19:41 -0700

    Refactor for loop syntax

--- index.php
+++ index.php
@@ -24,10 +24,9 @@
    ?>
 
    <ul>
-    <?php foreach ($books as $book) {
-        echo "<li>$book</li>";
-    }
-    ?>
+    <?php foreach ($books as $book): ?> 
+        <li><?php echo $book; ?> </li>
+    <?php endforeach; ?>
    </ul>
     
 </body>
